a007d1c63b1f3a3a1b75ff7f4147945ebdd16a39
lifthus <108582413+lifthus@users.noreply.github.com>
2022-11-21 00:45:18 +0900

    Create space_generator.py

---
+++ space_generator.py
@@ -0,0 +1,15 @@
+# e.g.
+# returns [[0,0],[0,1],[1,0],[1,1]] for length 2 and base [0,1]
+def space_generator_2d(length = 1, base=[0,1]):
+  space = []
+  for i in base:
+    space.append([i])
+  new_space = []
+  for i in range(length) :
+    for j in space:
+      for k in base:
+        new_space.append(j+[k])
+    if i!= length-1:
+      space = new_space[:]
+      new_space = []
+  return space
